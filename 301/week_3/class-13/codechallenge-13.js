642df42190858cf85eeab94f56cb7d3c93105ace
tektechnologies <info@tektechnologies.com>
2022-08-09 21:51:39 -0500

    add to code challenge demo

--- 301/week_3/class-13/codechallenge-13.js
+++ 301/week_3/class-13/codechallenge-13.js
@@ -1,18 +1,26 @@
 
+// https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/Array/every
+
+// https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/Array/includes
+
+//haystack includes needle.
+
 let str = 'My instructors shred';
 
 // includes
-console.log( str.includes('uct') );
+console.log('includes', str.includes('uct') );
 
 // substr
-console.log( str.substring(0,2) );
+console.log('substring', str.substring(0,9) );
 
 // charAt
-console.log( str.charAt(0) );
+console.log('charAt', str.charAt(0) );
 
 // charCodeAt
-console.log( str.charCodeAt(0) );
-console.log( 'A'.charCodeAt(0) );
+console.log( 'charCodeAt',str.charCodeAt(0) );
+console.log( 'charCodeAt: A ','A'.charCodeAt(0) );
 
-// fromCharCode
-console.log( String.fromCharCode(77) );
+// // fromCharCode
+console.log( 'fromCharCode(77)', String.fromCharCode(77) );
+
+// https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/String/charCodeAt
