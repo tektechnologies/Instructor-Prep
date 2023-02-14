80393933c608e4d88ad9c30e0f1c9f64d5118f13
tektechnologies <info@tektechnologies.com>
2023-02-14 13:11:47 -0600

    update code challeng demo

--- 301/week_3/class-12/codechallenge-12.js
+++ 301/week_3/class-12/codechallenge-12.js
@@ -7,3 +7,26 @@
 
 
 // (^(286)|^(\(286\)))-
+
+
+// look at the or
+// lots of ways to write numbers
+
+// (206) selects both but lets select more than those  ^(206)|^, which doesnt select the one in()
+
+// doesnt select 602 looking for literal number 
+
+// with square brackets ^[206]|^ is now looking for multiple numbers 
+ 
+// to look for 206 and parentheses  ^(206)|^(\(206\))
+
+// add the dash ^(206)|^(\(206\))-
+
+// select everything (^(206)|^(\(206\)))-
+
+// so we can keep digging down to find that number 
+// 026
+
+// 206-123-4567
+
+// (206)-123-4567
